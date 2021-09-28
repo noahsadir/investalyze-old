6a0f70ceff5d1ada5ce8a52c6668ac4b760b411e
noahsadir <development.noahsadir@gmail.com>
2021-09-28 15:08:47 -0400

    minor ui tweaks

--- src/components/SettingsDialog.js
+++ src/components/SettingsDialog.js
@@ -75,7 +75,7 @@ export default class SettingsDialog extends React.Component {
     const handleAPIKeyTextFieldChange = (event) => {
       this.setState({tempTradierKey: event.target.value});
     }
-    
+
     return (
       <Dialog
         open={this.props.open}
@@ -84,8 +84,10 @@ export default class SettingsDialog extends React.Component {
         <DialogTitle id="alert-dialog-title">{"Settings"}</DialogTitle>
         <DialogContent>
           <div style={{display: "flex", overflowY: "scroll", flexFlow: "column"}}>
-            <TextField size="small" style={{marginTop: 8, marginBottom: 8}} color="secondary" onChange={handleAPIKeyTextFieldChange} variant="outlined" label={"Tradier API Key"} placeholder={"Tradier API Key"} value={this.state.tempTradierKey}></TextField>
-            <Button style={{marginBottom: 8}} onClick={getTradierKeyButtonClicked} color={this.props.accentColor}>Get Tradier API Key</Button>
+            <div style={{display: "flex"}}>
+              <TextField size="small" style={{marginTop: 8, marginBottom: 8, flex: "1 0 0"}} color="secondary" onChange={handleAPIKeyTextFieldChange} variant="outlined" label={"Tradier API Key"} placeholder={"Tradier API Key"} value={this.state.tempTradierKey}></TextField>
+              <Button style={{marginBottom: 8, marginTop: 8, marginLeft: 8, minWidth: 96, flex: "0 0 0"}} variant="outlined" onClick={getTradierKeyButtonClicked} color={this.props.accentColor}>Get Key</Button>
+            </div>
             <div style={{display: "flex", marginBottom: 8, border: "1px solid " + this.props.theme.borderColor, borderRadius: 4}}>
               <p style={{flex: "1 0 0", width: 256, margin: 0, marginLeft: 16, marginTop: 12, lineHeight: "100%"}}>Dark Mode</p>
               <Switch onChange={darkModeSwitchChanged} checked={this.props.theme != null ? this.props.theme.darkMode : false} value="darkModeSwitch" inputProps={{ 'aria-label': 'Dark Mode' }} />
